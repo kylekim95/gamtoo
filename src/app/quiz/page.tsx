6756f4a8e9fee1f773cd2b60fe4ad8d45f3e384e
SangYupKim <kylekim95@gmail.com>
2025-01-22 11:58:18 +0900

    feat : ProblemCard 레이아웃

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import React, { useEffect, useRef, createRef } from 'react'
+import ProblemCard from './components/ProblemCard';
 
 export default function QuizPage() {
 
+  //TEST DATA
   const data : string[] = [
     'bg-red-900',
     'bg-red-800',
@@ -16,8 +18,9 @@ export default function QuizPage() {
     'bg-red-100',
     'bg-red-50'
   ];
-  const refs = useRef(data.map(()=>createRef<HTMLDivElement>()));
 
+  //TODO : Custom Hook
+  const refs = useRef(data.map(()=>createRef<HTMLDivElement>()));
   useEffect(()=>{
     const observer = new IntersectionObserver((entries)=>{
       entries.forEach((elem)=>{
@@ -31,6 +34,7 @@ export default function QuizPage() {
         }
       });
     }, { threshold: 0.5 });
+    console.log(refs);
     refs.current.forEach((elem)=>{
       if(elem.current !== null) observer.observe(elem.current);
     });
@@ -38,15 +42,11 @@ export default function QuizPage() {
 
   return (
     <div>
-      <div className='w-[100vw] h-[200px] ml-[-32px] mt-[-16px] bg-[#F0F0F0] flex justify-center items-center'>
-        <span className='text-[#FF8341] text-[64px] font-bold'>문화재 퀴즈</span>
+      <div className='w-[100vw] h-[200px] bg-[#F0F0F0] flex justify-center items-center'>
+        <span className='text-[#FF8341] text-[48px] font-bold'>문화재 퀴즈</span>
       </div>
       <div className='w-[100vw] mx-[-32px] flex flex-col items-center'>
-        {data.map((elem, index)=> <div className={
-          'w-[900px] h-[350px] m-1 ' +
-          'transition-opacity duration-300 ease-in-out ' +
-          elem
-        } key={index} ref={refs.current[index]}></div>)}
+        {data.map((elem, index)=><ProblemCard key={index} ref={refs.current[index]} /> )}
       </div>
     </div>
   )
