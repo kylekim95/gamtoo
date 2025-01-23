8602da508a9c6746e9d6bbe4c8a9d6d03a51fcc0
SangYupKim <kylekim95@gmail.com>
2025-01-23 10:53:07 +0900

    feat : 퀴즈 페이지 레이아웃 수정 및 버튼에 색 변경 애니메이션 추가

--- src/app/quiz/components/ProblemCard.tsx
+++ src/app/quiz/components/ProblemCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import CheckIcon from './svg/Checked'
-import CrossedIcon from './svg/Crossed'
+import CheckedIcon from './svg/CheckedIcon'
+import CrossedIcon from './svg/CrossedIcon'
 
 type ProblemCardProps = {
   ref: React.RefObject<HTMLDivElement | null>;
@@ -13,7 +13,7 @@ export default function ProblemCard(props : ProblemCardProps) {
     <div className='
       w-full max-w-[900px] min-w-[600px] aspect-[2.5/1] m-2
       transition-opacity duration-300 ease-in-out
-      rounded-lg drop-shadow-xl overflow-hidden
+      rounded-lg drop-shadow-2xl overflow-hidden
       flex
     ' ref={props.ref}>
       <div 
@@ -23,19 +23,19 @@ export default function ProblemCard(props : ProblemCardProps) {
         {/* <CheckIcon width={40} height={40} color={"#77FF77"}/> */}
         <CrossedIcon width={40} height={40} color={"#FF2222"}/>
       </div>
-      <div className='w-[90%] h-full flex flex-col'>
-        <div className='w-full h-[10%] min-h-[50px] flex items-center'>
+      <div className='w-[90%] h-full flex flex-col bg-slate-50'>
+        <div className='w-full h-[10%] min-h-[50px] flex items-end ml-5'>
           <span className='text-xl md:text-2xl text-black ml-2'>Q1.</span>
           <span className='text-sm md:text-lg text-black ml-2'>사진 속 문화재의 이름은?</span>
         </div>
-        <div className='w-full h-[90%] bg-slate-50 flex place-content-evenly items-center'>
-          <div className='w-[55%] aspect-[16/9] rounded-lg overflow-hidden grid grid-cols-2 gap-1'>
-            <div className='bg-black opacity-75 flex justify-center items-center'><span className='text-sm md:text-lg'>선택지 1</span></div>
-            <div className='bg-black opacity-75 flex justify-center items-center'><span className='text-sm md:text-lg'>선택지 2</span></div>
-            <div className='bg-black opacity-75 flex justify-center items-center'><span className='text-sm md:text-lg'>선택지 3</span></div>
-            <div className='bg-black opacity-75 flex justify-center items-center'><span className='text-sm md:text-lg'>선택지 4</span></div>
-          </div>
+        <div className='w-full h-[90%] flex place-content-evenly items-center'>
           <div className='w-[30%] aspect-square rounded-lg' style={{backgroundImage: `url(${props.url})`, backgroundSize: 'contain'}}></div>
+          <div className='w-[55%] aspect-[16/9] rounded-lg overflow-hidden grid grid-cols-2 gap-1'>
+            <div className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-red-700'><span className='text-sm md:text-lg'>선택지 1</span></div>
+            <div className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-blue-700'><span className='text-sm md:text-lg'>선택지 2</span></div>
+            <div className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-yellow-700'><span className='text-sm md:text-lg'>선택지 3</span></div>
+            <div className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-green-700'><span className='text-sm md:text-lg'>선택지 4</span></div>
+          </div>
         </div>
       </div>
     </div>
--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -41,12 +41,28 @@ export default function QuizPage() {
   }, [refs]);
 
   return (
-    <div className='flex flex-col items-center'>
-      <div className='w-[100vw] h-[200px] bg-[#F0F0F0] flex justify-center items-center'>
-        <span className='text-[#FF8341] text-[36px] md:text-[48px] font-bold'>문화재 퀴즈</span>
-      </div>
-      <div className='w-[75vw] flex flex-col items-center'>
-        {data.map((elem, index)=><ProblemCard key={index} ref={refs.current[index]} url={dummyImage} /> )}
+    <div>
+      <div className='flex flex-col items-center'>
+        <div className='w-[100vw] h-[200px] bg-[#F0F0F0] flex justify-center items-center'>
+          <span className='text-[#FF8341] text-[36px] md:text-[48px] font-bold'>문화재 퀴즈</span>
+        </div>
+        <div className='w-full flex'>
+          <div className='shrink w-[15%]'></div>
+          <div className='w-[70%] flex flex-col items-center'>
+            <div className='w-full flex flex-col items-center'>
+              {data.map((elem, index)=><ProblemCard key={index} ref={refs.current[index]} url={dummyImage} /> )}
+            </div>
+            <div className='w-full aspect-[6/1] flex justify-center items-center gap-10'>
+              <div className='h-[50%] aspect-square bg-black rounded-full flex justify-center items-center opacity-75 hover:bg-red-700 transition-colors'>
+                <span className=''>처음으로</span>
+              </div>
+              <div className='h-[50%] aspect-square bg-black rounded-full flex justify-center items-center opacity-75 hover:bg-blue-700 transition-colors'>
+                <span className=''>제출하기</span>
+              </div>
+            </div>
+          </div>
+          <div className='shrink w-[15%]'></div>
+        </div>
       </div>
     </div>
   )
