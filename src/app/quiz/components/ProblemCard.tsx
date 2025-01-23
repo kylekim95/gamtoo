c303c6471ff49c45b280aaea980d5690a386c798
SangYupKim <kylekim95@gmail.com>
2025-01-23 13:40:17 +0900

    feat : 퀴즈 페이지 선택지 고르는 기능

--- src/app/quiz/components/ProblemCard.tsx
+++ src/app/quiz/components/ProblemCard.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
-import CheckedIcon from '../../../components/quiz/svg/CheckedIcon'
-import CrossedIcon from '../../../components/quiz/svg/CrossedIcon'
-
 type ProblemCardProps = {
-  ref: React.RefObject<HTMLDivElement | null>;
-  url: string
+  id: string;
+  ref: React.RefObject<HTMLDivElement | null>; //Intersection Observer
+  url: string; //이미지 로딩 용
+  selectAnswer: string[];
+  selectAnswerCallback: (id: string, selected : number)=>void;
 }
 
 export default function ProblemCard(props : ProblemCardProps) {
+  function OnClickSelectBtn(selectedNum : number){
+    props.selectAnswerCallback(props.id, selectedNum);
+  }
+  
   return (
     <div className='
       w-full max-w-[900px] min-w-[600px] aspect-[2.5/1] m-2
@@ -20,8 +24,6 @@ export default function ProblemCard(props : ProblemCardProps) {
         className={`w-[10%] h-full flex justify-center items-center bg-[#00000080] rounded-lg`}
         style={{backgroundImage: `url(${props.url})`, backgroundSize:'cover', backgroundPosition: 'center', backgroundBlendMode: 'multiply'}}
       >
-        {/* <CheckIcon width={40} height={40} color={"#77FF77"}/> */}
-        <CrossedIcon width={40} height={40} color={"#FF2222"}/>
       </div>
       <div className='w-[90%] h-full flex flex-col bg-slate-50'>
         <div className='w-full h-[10%] min-h-[50px] flex items-end ml-5'>
@@ -31,10 +33,10 @@ export default function ProblemCard(props : ProblemCardProps) {
         <div className='w-full h-[90%] flex place-content-evenly items-center'>
           <div className='w-[30%] aspect-square rounded-lg' style={{backgroundImage: `url(${props.url})`, backgroundSize: 'contain'}}></div>
           <div className='w-[55%] aspect-[16/9] rounded-lg overflow-hidden grid grid-cols-2 gap-1'>
-            <div className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-red-700'><span className='text-sm md:text-lg'>선택지 1</span></div>
-            <div className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-blue-700'><span className='text-sm md:text-lg'>선택지 2</span></div>
-            <div className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-yellow-700'><span className='text-sm md:text-lg'>선택지 3</span></div>
-            <div className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-green-700'><span className='text-sm md:text-lg'>선택지 4</span></div>
+            <div onClick={()=>OnClickSelectBtn(0)} className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-red-700'><span className='text-sm md:text-lg'>{props.selectAnswer[0]}</span></div>
+            <div onClick={()=>OnClickSelectBtn(1)} className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-blue-700'><span className='text-sm md:text-lg'>{props.selectAnswer[1]}</span></div>
+            <div onClick={()=>OnClickSelectBtn(2)} className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-yellow-700'><span className='text-sm md:text-lg'>{props.selectAnswer[2]}</span></div>
+            <div onClick={()=>OnClickSelectBtn(3)} className='bg-black opacity-75 flex justify-center items-center transition-colors ease-in-out hover:bg-green-700'><span className='text-sm md:text-lg'>{props.selectAnswer[3]}</span></div>
           </div>
         </div>
       </div>
