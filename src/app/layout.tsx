633c506556bcd42748c3688cd49cd88ecc5e65e8
Jungseok Shin <jsjb7164@naver.com>
2025-01-31 16:12:27 +0900

    feat: 회원정보 페이지 라우터 연결, 수정 기능 구현
    1. 각시탈 아이콘 클릭시 회원 정보 페이지 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,12 +88,15 @@ interface props {
 function Header({ isNotification, notificationHandler }: props) {
   const router = useRouter();
 
-  const {isAuth, userId} = useAppSelector((state) => state.authReducer.value);
-  if(isNotification) {
+  const { isAuth, userId } = useAppSelector((state) => state.authReducer.value);
+  if (isNotification) {
     (async function () {
-      const response = await axios.get(`${process.env.NEXT_PUBLIC_BASIC_URL}/notifications`,{headers: {"Content-type": "application/json"}});
-      console.log("response",response)
-    })()
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BASIC_URL}/notifications`,
+        { headers: { "Content-type": "application/json" } }
+      );
+      console.log("response", response);
+    })();
   }
 
   return (
@@ -188,24 +191,24 @@ function Header({ isNotification, notificationHandler }: props) {
               className="size-10 mr-4"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
           )}
-          <div className="w-[75px] h-[75px] rounded-full border-4 overflow-hidden content-center border-black">
+          <div
+            onClick={() => {
+              router.push("/user");
+            }}
+            className="w-[75px] h-[75px] rounded-full border-4 overflow-hidden content-center border-black hover:cursor-pointer"
+          >
             <GagsiMaskIcon width={68} height={70} color={"#00000"} />
           </div>
         </div>
       ) : (
         <div className="right mt-1 flex flex-row items-center gap-5">
-          <UserCircleIcon
-            onClick={() => {
-              router.push("/user");
-            }}
-            className="size-8 text-black hover:cursor-pointer"
-          />
+          <UserCircleIcon className="size-8 text-black hover:cursor-pointer" />
           <span
             onClick={() => {
               router.push("/login");
